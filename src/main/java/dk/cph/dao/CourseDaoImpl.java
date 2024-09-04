816d65c7b2eac7d6ca268dcd6cd6a15d2e930eb9
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-09-04 10:39:01 +0200

    Added entities and enum.

--- src/main/java/dk/cph/dao/CourseDaoImpl.java
+++ src/main/java/dk/cph/dao/CourseDaoImpl.java
@@ -1,12 +1,19 @@
 package dk.cph.dao;
 
+import dk.cph.model.Course;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CourseDaoImpl {
+public class CourseDaoImpl implements GenericDAO<Course> {
 
     private static CourseDaoImpl instance;
     private static EntityManagerFactory emf;
@@ -17,5 +24,40 @@ public class CourseDaoImpl {
             instance = new CourseDaoImpl();
         }
         return instance;
+    }
+
+    @Override
+    public Set<Course> findAll(int maxResults) {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Course> query = em.createQuery("SELECT c FROM Course c", Course.class)
+                    .setMaxResults(maxResults);
+            return query.getResultList().stream().collect(Collectors.toSet());
+        }
+    }
+
+    @Override
+    public void removeEntity(Course id) {
+
+    }
+
+    @Override
+    public Course findEntity(Course id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            return em.find(Course.class, id);
+        }
+    }
+
+    @Override
+    public Course updateEntity(Object entity, Object id) {
+        return null;
+    }
+
+    @Override
+    public void persistEntity(Course course, ) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(course);
+            em.getTransaction().commit();
+        }
     }
 }
