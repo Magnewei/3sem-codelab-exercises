a90ca12a38289db7b6e74021f4182ddc28ed33c8
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-09-04 10:22:04 +0200

    Added entities and enum.

--- src/main/java/dk/cph/model/Student.java
+++ src/main/java/dk/cph/model/Student.java
@@ -4,13 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "students")
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 @NamedQueries({
         @NamedQuery(name = "Student.findAll", query = "SELECT s FROM Student s"),
         @NamedQuery(name = "Student.findByEmail", query = "SELECT s FROM Student s WHERE s.email = :email"),
@@ -36,5 +35,14 @@ public class Student {
     @Setter(AccessLevel.NONE)
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "student_course",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private Set<Course> courses = new HashSet<>();
+
+
 
 }
--- src/main/java/dk/cph/model/Teacher.java
+++ src/main/java/dk/cph/model/Teacher.java
@@ -3,29 +3,24 @@ package dk.cph.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 
 @Entity
 @Table(name = "teachers")
-@NoArgsConstructor
-@Getter
-@ToString
+@Data
 @NamedQuery(name = "Teacher.deleteAll", query = "DELETE FROM Teacher")
 public class Teacher {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "email", nullable = false, unique = true)
     private String email;
-
-    @Column(name = "zoom", unique = true)
     private String zoom;
 
-    @ManyToOne
-    private Course course;
-
+    @OneToMany(mappedBy = "teacher")
+    private Set<Course> courses;
 }
