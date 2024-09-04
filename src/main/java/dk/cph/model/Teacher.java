a6b1479e5953fabffdd845c67fd77b6cebf2702b
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-09-04 10:10:21 +0200

    Add pom.xml

--- src/main/java/dk/cph/model/Teacher.java
+++ src/main/java/dk/cph/model/Teacher.java
@@ -3,24 +3,29 @@ package dk.cph.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 
 @Entity
-@Table(name = "teacher")
-@Data
+@Table(name = "teachers")
+@NoArgsConstructor
+@Getter
+@ToString
 @NamedQuery(name = "Teacher.deleteAll", query = "DELETE FROM Teacher")
 public class Teacher {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "zoom", unique = true)
     private String zoom;
 
-    @OneToMany(mappedBy = "teacher")
-    private Set<Course> courses;
+    @ManyToOne
+    private Course course;
+
 }
