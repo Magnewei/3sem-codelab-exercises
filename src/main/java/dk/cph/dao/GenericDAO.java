609e121a78b2985315849b3e8e1ef14502ec6792
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-09-04 10:39:01 +0200

    Added entities and enum.

--- src/main/java/dk/cph/dao/GenericDAO.java
+++ src/main/java/dk/cph/dao/GenericDAO.java
@@ -1,13 +1,12 @@
 package dk.cph.dao;
 
 import java.util.List;
+import java.util.Set;
 
 public interface GenericDAO<T, D> {
-
-    List<T> findAll();
+    Set<T> findAll(int amount);
     void persistEntity(T entity);
     void removeEntity(D id);
     T findEntity(D id);
     T updateEntity(T entity, D id);
-
 }
